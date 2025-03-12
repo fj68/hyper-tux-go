541fa468d14536264a2c6ba36cd1a708f8de42d1
Shintaro K <fj68forg@gmail.com>
2025-03-12 23:58:41 +0000

    add test for slicetools

---
+++ slicetools/slicetools_test.go
@@ -0,0 +1,47 @@
+package slicetools_test
+
+import (
+	"testing"
+
+	"github.com/fj68/hyper-tux-go/slicetools"
+)
+
+func TestEvery(t *testing.T) {
+	testdata := []int{1, 2, 3, 4, 5}
+
+	isLessThanTen := func(n int) bool {
+		return n < 10
+	}
+
+	isEven := func(n int) bool {
+		return n%2 == 0
+	}
+
+	if slicetools.Every(testdata, isEven) {
+		t.Errorf("all numbers are even")
+	}
+
+	if !slicetools.Every(testdata, isLessThanTen) {
+		t.Errorf("not all numbers are less than 10")
+	}
+}
+
+func TestSome(t *testing.T) {
+	testdata := []int{1, 2, 3, 4, 5}
+
+	isLessThanZero := func(n int) bool {
+		return n < 0
+	}
+
+	isEven := func(n int) bool {
+		return n%2 == 0
+	}
+
+	if !slicetools.Some(testdata, isEven) {
+		t.Errorf("all numbers are not even")
+	}
+
+	if slicetools.Some(testdata, isLessThanZero) {
+		t.Errorf("some numbers are less than 0")
+	}
+}
