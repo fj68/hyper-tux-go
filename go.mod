3d9b3be7d56236c993b394c42b2944486d941836
Shintaro K <fj68forg@gmail.com>
2025-03-13 06:07:24 +0000

    add weighted random choicer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fj68/hyper-tux-go
 
 go 1.23.4
+
+require golang.org/x/exp v0.0.0-20250305212735-054e65f0b394
---
+++ hyper/random.go
@@ -0,0 +1,22 @@
+package hyper
+
+import (
+	"math/rand"
+
+	"github.com/fj68/hyper-tux-go/slicetools"
+)
+
+// weighted random choice using linear scan algorithm
+func Choice[T any](candidates []T, weights []int) T {
+	total := slicetools.Sum(weights)
+	r := rand.Intn(total)
+
+	for i, w := range weights {
+		r -= w
+		if r < 0 {
+			return candidates[i]
+		}
+	}
+	var zero T
+	return zero
+}
