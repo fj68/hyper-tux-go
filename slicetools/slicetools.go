a5e8a756dcbccbe16c9e443abb753f180eff9d5a
Shintaro K <fj68forg@gmail.com>
2025-03-13 06:06:55 +0000

    add slicetools.Sum

--- slicetools/slicetools.go
+++ slicetools/slicetools.go
@@ -1,5 +1,9 @@
 package slicetools
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
 func Every[T any](xs []T, f func(T) bool) bool {
 	for _, v := range xs {
 		if !f(v) {
@@ -16,4 +20,12 @@ func Some[T any](xs []T, f func(T) bool) bool {
 		}
 	}
 	return false
+}
+
+func Sum[T constraints.Integer | constraints.Float](xs []T) T {
+	var total T
+	for _, x := range xs {
+		total += x
+	}
+	return total
 }
