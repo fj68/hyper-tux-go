672809769334bf31ef6518c0e6ccc9e168abe6d8
Shintaro K <fj68forg@gmail.com>
2025-03-13 00:11:50 +0000

    add test for Goal#Reached

---
+++ hyper/goal_test.go
@@ -0,0 +1,30 @@
+package hyper_test
+
+import (
+	"testing"
+
+	"github.com/fj68/hyper-tux-go/hyper"
+)
+
+func TestGoal(t *testing.T) {
+	goal := hyper.Goal{hyper.Red, hyper.Point{10, 5}}
+	
+	testcases := []struct {
+		Name string
+		hyper.Actor
+		Expected bool
+	} {
+		{"Reached", hyper.Actor{hyper.Red, hyper.Point{10, 5}}, true},
+		{"Different color", hyper.Actor{hyper.Blue, hyper.Point{10, 5}}, false},
+		{"Different row", hyper.Actor{hyper.Red, hyper.Point{10, 4}}, false},
+		{"Different column", hyper.Actor{hyper.Red, hyper.Point{9, 5}}, false},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.Name, func(t *testing.T) {
+			if goal.Reached(testcase.Actor) != testcase.Expected {
+				t.Errorf("unexpected value in case %s", testcase.Name)
+			}
+		})
+	}
+}
