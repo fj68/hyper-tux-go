cd72b1e53f1e07fdfcae8a311c1b7b99b9332779
Shintaro K <fj68forg@gmail.com>
2025-03-17 05:18:26 +0000

    change to pass values not by pointer

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import "github.com/fj68/hyper-tux-go/hyper"
+
+const CELL_SIZE = 20 // px
+
+func main() {
+	board, err := hyper.NewBoard(&hyper.Size{W: 16, H: 16})
+	if err != nil {
+		panic(err)
+	}
+	if err = board.NewGame(); err != nil {
+		panic(err)
+	}
+}
