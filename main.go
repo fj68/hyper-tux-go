dce0addde1fbcdf75ef46ada257a70bf4608e07d
Shintaro K <fj68forg@gmail.com>
2025-03-17 05:19:21 +0000

    change to pass values by pointer

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/fj68/hyper-tux-go/hyper"
 const CELL_SIZE = 20 // px
 
 func main() {
-	board, err := hyper.NewBoard(&hyper.Size{W: 16, H: 16})
+	board, err := hyper.NewBoard(hyper.Size{W: 16, H: 16})
 	if err != nil {
 		panic(err)
 	}
