b8b7c3df2bd158b5b5118fb05522810fba129205
Shintaro K <fj68forg@gmail.com>
2025-03-21 04:17:30 +0000

    add Actor#Equals

--- hyper/actor.go
+++ hyper/actor.go
@@ -5,6 +5,10 @@ type Actor struct {
 	Point
 }
 
+func (a *Actor) Equals(other Actor) bool {
+	return a.Color == other.Color && a.Point.Equals(other.Point)
+}
+
 func (a *Actor) MoveTo(p Point) {
 	a.Point = p
 }
